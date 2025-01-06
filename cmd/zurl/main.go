0733aceec513dc43023299ceada27667c5c15f62
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2025-01-06 19:49:41 +0300

    Add route to make a short URL. Accepts long url from form, calculate hash and return first 6 symbols as a short link. Write short url as a key with long url as a value to cache (just a map now)

--- cmd/zurl/main.go
+++ cmd/zurl/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"crypto/sha256"
+    "encoding/hex"
+    "net/http"
+)
+
+var cache = make(map[string]string)
+
+func short(w http.ResponseWriter, r *http.Request) {
+	longURL := r.FormValue("longURL")
+
+	hash := sha256.New()
+    hash.Write([]byte(longURL))
+    hashed := hash.Sum(nil)
+
+	shortURL := hex.EncodeToString(hashed)[:6]
+
+	cache[shortURL] = longURL
+
+	w.Write([]byte(shortURL))
+}
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/v1/short", short)
 
 	http.ListenAndServe(":8080", mux)
 }
